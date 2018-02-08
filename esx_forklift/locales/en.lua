24739f6d0b6d58c60d57d3fbedbb3b1144248e9b
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-02-08 16:48:22 +0200

    Update en.lua

--- esx_forklift/locales/en.lua
+++ esx_forklift/locales/en.lua
@@ -6,7 +6,7 @@
 --					and the string below will be displayed to the player
 
 Locales['en'] = { --Locales['x'], replace is x with your language. For example 'fr' and set Config.Locale = ['fr'] in config.lua
-
+		  --You will also have to add your localization file to the __resource.lua
     ['not_enough'] = 'You do not have enough money, you poor bastard.',
 	['another_example'] = 'more strings'
 
