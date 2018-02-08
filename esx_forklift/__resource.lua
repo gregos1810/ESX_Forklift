07567f518300d6a9e32c28284bebcb3f501a68fa
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-02-08 16:48:44 +0200

    Update __resource.lua

--- esx_forklift/__resource.lua
+++ esx_forklift/__resource.lua
@@ -3,20 +3,14 @@ version '1.1.0'
 
 client_scripts {
     '@es_extended/locale.lua',
-    'locales/de.lua',
-    'locales/br.lua',
     'locales/en.lua',
-    'locales/fr.lua',
     'config.lua',
     'client/main.lua'
 }
 
 server_scripts {
     '@es_extended/locale.lua',
-    'locales/de.lua',
-    'locales/br.lua',
     'locales/en.lua',
-    'locales/fr.lua',
     'config.lua',
     'server/main.lua'
 }
